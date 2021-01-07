9b73872bfec2e928c14c4197170633a72b4f0044
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-01-07 10:27:19 +0100

    Create TextBook.java

---
+++ Java-Helion/2/TextBook.java
@@ -0,0 +1,33 @@
+public class TextBook
+{
+    private String title;
+    private String author;
+    private String publisher;
+
+    public TextBook(String textTitle, String auth, String pub)
+    {
+        title = textTitle;
+        author = auth;
+        publisher = pub;
+    }
+
+    public TextBook(TextBook object2)
+    {
+        title = object2.title;
+        author = object2.author;
+        publisher = object2.publisher;
+    }
+
+    public void ser(String textTitle, String auth, String pub)
+    {
+        title = textTitle;
+        author = auth;
+        publisher = pub;
+    }
+    public String toString()
+    {
+        String str = "Tytuł: " + title + "\nAutor: " + author + "\nWydawnictwo: " + publisher;
+
+        return str;
+    }
+}
