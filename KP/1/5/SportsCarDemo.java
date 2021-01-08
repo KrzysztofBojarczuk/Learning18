61046e9cf29b5b17f3a371c03baf3d7e277802dd
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-01-08 11:31:07 +0100

    Create SportsCarDemo.java

---
+++ KP/1/5/SportsCarDemo.java
@@ -0,0 +1,8 @@
+public class SportsCarDemo
+{
+    public static void main(String[] args) {
+        SportsCar yoursNewCar = new SportsCar(CarType.PORSCHE, CarColor.CZERWONY, 100000);
+
+        System.out.println(yoursNewCar);
+    }
+}
