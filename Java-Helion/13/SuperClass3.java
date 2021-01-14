e3c418c012f34d631f46fa66ec590ac83c1129b4
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-01-14 10:09:32 +0100

    Create SuperClass3.java

---
+++ Java-Helion/13/SuperClass3.java
@@ -0,0 +1,13 @@
+public class SuperClass3
+{
+    public void showValue(int arg)
+    {
+        System.out.println("KLASA BAZOWA: " + "Arguemnt typu int to: " + arg);
+    }
+
+    public void showValue(String arg)
+
+    {
+        System.out.println("KLASA BAZOWA: " + "Arguemnt typu String to: " + arg);
+    }
+}
