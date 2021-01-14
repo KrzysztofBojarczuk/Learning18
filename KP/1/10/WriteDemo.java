8d9c2744aa295b81d95d2d5b5af303407b8b1729
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-01-14 16:13:05 +0100

    Create WriteDemo.java

---
+++ KP/1/10/WriteDemo.java
@@ -0,0 +1,10 @@
+public class WriteDemo
+{
+    public static void main(String[] args) {
+        int b;
+
+        b = 'A';
+        System.out.println(b);
+        System.out.println('\n');
+    }
+}
