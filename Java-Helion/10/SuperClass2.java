1b58c1fcd75114180601fef5982d00e35332e584
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-01-14 09:18:09 +0100

    Create SuperClass2.java

---
+++ Java-Helion/10/SuperClass2.java
@@ -0,0 +1,13 @@
+public class SuperClass2
+{
+    public SuperClass2()
+    {
+        System.out.println("To konstruktor bezarguemntowy" + "klasy bazowej.");
+    }
+
+    public SuperClass2(int arg)
+    {
+        System.out.println("Do konstruktora " + "klasy bazowej przekazano " +
+                "następujące arguemnty: " + arg);
+    }
+}
