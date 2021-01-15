2041207713ed7d164fa95d26f40285b899a26355
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-01-15 08:59:58 +0100

    Create GradedActivity2.java

---
+++ Java-Helion/13/GradedActivity2.java
@@ -0,0 +1,31 @@
+public class GradedActivity2
+{
+    protected double score;
+
+    public void setScore(double s)
+    {
+        score = s;
+    }
+    public double getScore()
+    {
+        return score;
+    }
+
+    public char getGrade()
+    {
+        char letterGrade;
+
+        if (score >= 90)
+            letterGrade = '5';
+        else if (score >= 80)
+            letterGrade = '4';
+        else if (score >= 70)
+            letterGrade = '3';
+        else if (score >= 60)
+            letterGrade = '2';
+        else
+            letterGrade = '1';
+
+        return letterGrade;
+    }
+}
