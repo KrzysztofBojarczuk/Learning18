b8b84ffdfbe13a24283a0ea64f50b4cbcf0e3800
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-01-16 11:15:43 +0100

    Create TwoGen.java

---
+++ KP/1/11/TwoGen.java
@@ -0,0 +1,22 @@
+public class TwoGen<T, V>
+{
+    T ob1;
+    V ob2;
+
+    TwoGen(T o1, V o2) {
+        ob1 = o1;
+        ob2 = o2;
+    }
+
+    void showTypes() {
+        System.out.println("Typ T to " + ob1.getClass().getName());
+
+        System.out.println("Typ V to " + ob2.getClass().getName());
+    }
+    T getob1() {
+        return ob1;
+    }
+    V getob2() {
+        return ob2;
+    }
+}
